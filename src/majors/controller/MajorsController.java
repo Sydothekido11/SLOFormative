cb90dff8e4efcdceb8780277d6037cb5eaf4d2bf
Sydothekido11 <snem8901@740s-pl-109.local>
2016-03-23 12:27:02 -0600

    Started the controller.

--- src/majors/controller/MajorsController.java
+++ src/majors/controller/MajorsController.java
@@ -1,5 +1,54 @@
 package majors.controller;
 
-public class MajorsController {
+import majors.model.MajorsModel;
+import majors.view.MajorsDisplay;
+import majors.view.MajorsFrame;
 
+
+public class MajorsController 
+{
+
+	private MajorsModel myMajors;
+	private MajorsDisplay myDisplay;
+	private MajorsFrame majorsFrame;
+	
+	public MajorsController()
+	{
+		
+		myDisplay = new MajorsDisplay();
+		String user = myDisplay.getUserInput("Enter your name!");
+		myMajors = new MajorsModel(user);
+		majorsFrame = new MajorsFrame(this);
+		
+	}
+	
+	public void start()
+	{
+		myDisplay.displayResponse("Hello " + myMajors.getUserName());
+	}
+	
+	private void 
+	{
+		String textFromUser - myDisplay.getUserInput("");
+		while(myMajors.lengthChecker(textFromUser))
+		{
+			textFromUser = myMajors.processQuestion(textFromUser);
+			textFromUser = myDisplay.getUserInput(textFromUser);
+		}
+	}
+	
+	public MajorsModel getMajorsModel()
+	{
+		return null;
+	}
+	
+	public MajorsDisplay getMajorsDisplay()
+	{
+		return null;
+	}
+	
+	public MajorsFrame getMajorsFrame()
+	{
+		return null;
+	}
 }
