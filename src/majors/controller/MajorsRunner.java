ffe5958b307fb023b032366b2d73c81252eecf3d
Sydothekido11 <snem8901@740s-pl-109.local>
2016-03-23 12:09:45 -0600

    Added the runner.

--- src/majors/controller/MajorsRunner.java
+++ src/majors/controller/MajorsRunner.java
@@ -1,5 +1,11 @@
 package majors.controller;
 
-public class MajorsRunner {
+public class MajorsRunner 
+{
 
+	public static void main (String [] args)
+	{
+		MajorsController myMajorsController = new MajorsController();
+		myMajorsController.start();
+	}
 }
